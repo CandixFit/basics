0601f428d820ad8a25e112651919750c7fff7919
CandixFit <can.cngz@outlook.de>
2025-04-14 12:32:37 +0200

    Create TriviaBotfCC.js
    simple console logs and setting new values to same variable

---
+++ TriviaBotfCC.js
@@ -0,0 +1,25 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName;
+let botLocation;
+let favoriteLanguage;
+
+botName = "Jan";
+botLocation = "Sylt";
+favoriteLanguage = "Python";
+
+const botIntroduction = "My name is " + botName + " and I live on " + botLocation + ".";
+console.log(botIntroduction);
+
+const botLanguage = "My favorite programming language is " + favoriteLanguage + ".";
+console.log(botLanguage);
+
+let codingFact = "My favorite language is " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "did you know," + favoriteLanguage + " is also a snake?";
+console.log(codingFact);
+
+codingFact = "What else can i say about " + favoriteLanguage + "?";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
